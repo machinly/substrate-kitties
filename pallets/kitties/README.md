74c91da48f86e6a71f697926c4647b8dd337b49b
machinly <machinlyg@gmail.com>
2024-01-05 23:26:49 +0800

    feat: add test read me

--- pallets/kitties/README.md
+++ pallets/kitties/README.md
@@ -1,1 +1,8 @@
 README
+
+1. 单元测试截图
+ 
+![单元测试截图](./image/test.png)
+ 
+2. Node 运行截图
+![Node 运行截图](./image/node.png)
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -48,6 +48,8 @@ pub use sp_runtime::{Perbill, Permill};
 /// Import the template pallet.
 pub use pallet_template;
 
+pub use pallet_kitties;
+
 /// An index to a block.
 pub type BlockNumber = u32;
 
@@ -268,6 +270,12 @@ impl pallet_template::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 }
 
+use pallet_insecure_randomness_collective_flip;
+impl pallet_kitties::Config for Runtime {
+	type Event = RuntimeEvent;
+	type Randomness = pallet_insecure_randomness_collective_flip;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub struct Runtime
@@ -285,6 +293,7 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		KittiesModule: pallet_kitties,
 	}
 );
 
